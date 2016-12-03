7a4b0b0c526e5f44b75823ee8d00ffc616791580
Matt Coles <mattc65@gmail.com>
2016-12-03 13:17:55 +0000

    Tidy up day3 code

--- day3/main2.js
+++ day3/main2.js
@@ -1,23 +1,26 @@
 const fs = require('fs')
 const input = fs.readFileSync(process.argv[2], { encoding: 'utf-8' })
 const validTriangles = t => (t[0]+t[1])>t[2]
-const emptySpaces = t => !!t
-const toColumns = (_, idx, arr) => ((idx % 3 === 0 || idx === 0) && idx+1 != arr.length) ? 
+const toColumns = (_, idx, arr) => idx % 3 === 0 ? 
   [
     [arr[idx][0], arr[idx+1][0], arr[idx+2][0]],
     [arr[idx][1], arr[idx+1][1], arr[idx+2][1]],
     [arr[idx][2], arr[idx+1][2], arr[idx+2][2]]
   ] :  ''
 
-const inputArray = input.split('\n').map(e =>
-  e.split(" ")
-  .filter(emptySpaces)
-  .map(t => +t))
-    .filter(e => e.length)
-    .map(toColumns)
-    .filter(emptySpaces)
-    .reduce((p, c) => p.concat(c), [])
-    .map(e => e.sort((a,b) => a-b))
-    .filter(validTriangles)
+const inputArray = 
+  input
+  .trim()
+  .split('\n')
+  .map(e => e
+    .trim()
+    .split(/\s+/)
+    .map(t => +t))
+  .map(toColumns)
+  .filter((_, idx) => !(idx % 3))
+  .reduce((p, c) => p.concat(c), [])
+  .map(e => e
+    .sort((a,b) => a-b))
+  .filter(validTriangles)
 
 console.log(inputArray.length)
