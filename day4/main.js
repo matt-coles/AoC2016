709b294639f294a82f97118a4d23a4c0c55c14a0
Matt Coles <mattc65@gmail.com>
2016-12-04 15:11:30 +0000

    Add day4 part2 solution in main.js

--- day4/main.js
+++ day4/main.js
@@ -1,6 +1,16 @@
 const fs = require('fs')
 const input = fs.readFileSync(process.argv[2], { encoding: 'utf-8' })
 const frequency = (el, arr) => arr.filter(e => e == el).length
+const sectorID = e => e.replace(/\D+(\d+)\D+/g, '$1')
+const ciphered = e => e.replace(/(\D+)-\d.*/g, '$1')
+const shift = (word, amt) => word.split('').map(a => {
+  if (a === '-') return ' '
+  else {
+    const code = a.charCodeAt(0)
+    if (code+(amt%26) > 122) return String.fromCharCode(code+(amt%26)-26)
+    else return String.fromCharCode(code+(amt%26))
+  }
+}).join('')
 
 const inputArray = input.trim().split('\n')
 const encryption = inputArray
@@ -21,8 +31,16 @@ const encryption = inputArray
     .join(''))
 
 const validRooms = inputArray.filter((e, idx) => e.replace(/.+\[(\w+)\]/g, '$1') === encryption[idx])
-const sectorIDSum = validRooms.map(e => e.replace(/\D+(\d+)\D+/g, '$1'))
+const sectorIDSum = validRooms.map(sectorID)
   .map(e => +e)
   .reduce((p, c) => p+c,0)
 
 console.log("Sector ID sum: " + sectorIDSum) // Part 1 solution
+
+const actualNames = validRooms
+  .map(e => { return { c: ciphered(e), n: sectorID(e) } })
+  .map(e => { return { c: shift(e.c, e.n), n: e.n }})
+  .filter(e => !!~e.c.indexOf('northpole'))
+
+console.log(actualNames) // Part 2 solution
+
